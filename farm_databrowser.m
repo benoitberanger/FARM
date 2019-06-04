d1075e05fab680f0d15018fb7dfd09def0c62604
Benoît Béranger <benoit.beranger@icm-institute.org>
2019-06-04 11:16:11 +0200

    carpet plot uses the last *_clean data

--- farm_databrowser.m
+++ farm_databrowser.m
@@ -1,6 +1,6 @@
 function farm_databrowser( data, filter )
 % FARM_DATABROWSER will use ft_databrowser on the most recent *_clean field
-% 
+%
 % See also ft_databrowser farm_filter
 
 if nargin==0, help(mfilename); return; end
@@ -8,10 +8,11 @@ if nargin==0, help(mfilename); return; end
 
 %% Prepare data
 
+% Fetch all *_clean fields name
 field_name = fieldnames( data );
+clean_idx  = find(~cellfun(@isempty, strfind(field_name,'_clean'))); %#ok<STRCLFH>
 
-clean_idx = find(~cellfun(@isempty, strfind(field_name,'_clean'))); %#ok<STRCLFH>
-
+% Use the last *_clean field
 if ~isempty(clean_idx)
     data.trial{1} = data.(field_name{clean_idx(end)});
 end
--- farm_plot.m
+++ farm_plot.m
@@ -1,9 +1,9 @@
-function farm_carpet_plot( data, filter )
-% FARM_CARPET_PLOT will plot the volume-segments of a channel
+function farm_plot( data, filter )
+% FARM_PLOT will plot the data inside the volume markers
 % The volume markers will be 'data.volume_marker_name'
 % The channel will be detected by farm_detect_channel_with_greater_artifact
 %
-% Syntax : FARM_CARPET_PLOT( data, filter )
+% Syntax : FARM_PLOT( data, filter )
 %
 % See also farm_filter
 
@@ -35,24 +35,15 @@ if nargin > 1
     channel = farm_filter(channel, data.fsample, filter);
 end
 
+
 volume_event = ft_filter_event( data.cfg.event, 'value', data.volume_marker_name );
-
-
-%% Prepare the carpet
-
-volume_segement = zeros(length(volume_event), data.sequence.TR * data.fsample);
-
-for iVol = 1 : length(volume_event)
-    volume_segement( iVol, : ) = channel( volume_event(iVol).sample : volume_event(iVol).sample + data.sequence.TR * data.fsample -1 );
-end
+channel      = channel(volume_event(1).sample : volume_event(end).sample);
 
 
 %% Plot
 
-figure('Name',sprintf('Carpet plot @ channel %d',data.target_channel),'NumberTitle','off');
-image(volume_segement,'CDataMapping','scaled')
-colormap(gray(256))
-colorbar
+figure('Name',sprintf('Plot @ channel %d',data.target_channel),'NumberTitle','off');
+plot( (0:length(channel)-1)/data.fsample , channel )
 
 
 end % function
