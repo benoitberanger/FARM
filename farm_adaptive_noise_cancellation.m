ea4d05c81cbbdefb76fd53b15b7cfd7ef8640fa5
Benoît Béranger <benoit.beranger@icm-institute.org>
2019-06-04 16:07:51 +0200

    renamed fastranc* into farm_fastranc* to avoid collisions

--- farm_adaptive_noise_cancellation.m
+++ farm_adaptive_noise_cancellation.m
@@ -70,10 +70,10 @@ for iChannel = 1 : nChannel
     alpha = sum(d .* refs) / sum( refs .* refs );
     refs  = alpha * refs;
     
-    N  = round(sdur*fsample);
-    mu = 0.05 / (N * var(refs));
+    N  = round(sdur*fsample);    % filter weigths
+    mu = 0.05 / (N * var(refs)); % filter step size
     
-    [~,y]=fastranc(refs,d,N,mu);
+    [~,y]=fast_fastranc(refs,d,N,mu);
     
     % Store
     if max(y) > 1e6 % works better than isinf
--- farm_fastranc.m
+++ farm_fastranc.m
@@ -53,7 +53,7 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function [out,y]=fastranc(refs,d,N,mu)
+function [out,y]=farm_fastranc(refs,d,N,mu)
 
 nargchk(4,4,nargin);
 refs=refs(:);
