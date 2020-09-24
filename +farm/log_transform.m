8f71a24769d6ff015ca7e392e42e8b0646677245
Benoît Béranger <benoit.beranger@icm-institute.org>
2020-09-24 20:13:16 +0200

    log_transform correction, better for regressor generation

--- +farm/log_transform.m
+++ +farm/log_transform.m
@@ -11,15 +11,19 @@ function output = log_transform( input )
 
 if nargin==0, help(mfilename('fullpath')); return; end
 
+
+%%
+
 output = input;
 
 neg_val_idx = output < 0;
 if any(neg_val_idx)
     output = output - min(output(neg_val_idx)); % shift the curve up, so there is no negative values
 end
-output = farm.normalize_range(output)+eps;   % normalize & avoid having a 0, which would give log(0)=-Inf
+output = farm.normalize_range(output);       % normalize & avoid having a 0, which would give log(0)=-Inf
 output = log(output);                        % log transform
-output = output - min(output);                  % shift down, so the minimum is 0
+output(output==-Inf) = min(output(output~=-Inf)); % remove -Inf
+output = output - min(output);               % shift up
 output = farm.normalize_range(output);       % normalize
 
 
